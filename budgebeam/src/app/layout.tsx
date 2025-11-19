7d4229cd263cec7bb7429a1a425064143384af10
kukin01 <nicolenezaa12@gmail.com>
2025-11-19 15:44:55 +0200

    initial commit && finish socket.io-client setup

---
+++ budgebeam/src/app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { SocketProvider } from "@/context/SocketContext";
+
+
+export const metadata: Metadata = {
+  title: "BudgeBeam",
+  description: "Build your own budges"
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body>
+        <SocketProvider>
+          {children}
+        </SocketProvider>
+      </body>
+    </html>
+  );
+}
